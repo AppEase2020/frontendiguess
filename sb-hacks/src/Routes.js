df2a1e27c0ab4345026488fadfc8f3c5614cf01d
achyuthv99 <achyuth@ucsb.edu>
2020-01-11 20:37:02 -0800

    second commit

--- sb-hacks/src/Routes.js
+++ sb-hacks/src/Routes.js
@@ -3,12 +3,14 @@ import { Route, Switch } from "react-router-dom";
 import Home from "./containers/Home";
 import NotFound from "./containers/NotFound";
 import Login from "./containers/Login";
+import Signup from "./containers/Signup";
 
 export default function Routes() {
   return (
     <Switch>
       <Route path="/" exact component={Home} />
       <Route path="/login" exact component={Login} />
+      <Route path="/signup" exact component={Signup} />
       {/* Finally, catch all unmatched routes */}
       <Route component={NotFound} />
     </Switch>
