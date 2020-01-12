43b36b1d848265a463d475eb02d22a69e686a79e
Jay Patwardhan <jaypatwardhan@169-231-113-168.wireless.ucsb.edu>
2020-01-11 22:22:28 -0800

    starting connecting frontend and backend

--- sb-hacks/src/containers/Signup.js
+++ sb-hacks/src/containers/Signup.js
@@ -1,17 +1,31 @@
 import React from "react";
 import { MDBContainer, MDBRow, MDBCol, MDBBtn } from "mdbreact";
 
+async function submitdata(event){
+  event.preventDefault();
+  console.log(event.target);
+  const form = event.target;
+  const data = new FormData(form);
+  const object = {}
+  data.forEach(function(value, key){
+    object[key] = value;
+  });
+  const json = JSON.stringify(object);
+  const response = await fetch('http://localhost:3000/api/users', {method: 'POST', headers: {'Content-Type': 'application/json'}, body: json});
+  const jsonResponse = await response.json();
+  console.log(jsonResponse);
+}
 const Signup = () => {
   return (
     <MDBContainer>
       <MDBRow>
         <MDBCol md="6">
-          <form>
+          <form onSubmit ={submitdata}>
             <p className="h4 text-center mb-4">Sign up</p>
             <label htmlFor="defaultFormRegisterNameEx" className="grey-text">
               Your name
             </label>
-            <input
+            <input name = 'name'
               type="text"
               id="defaultFormRegisterNameEx"
               className="form-control"
@@ -20,7 +34,8 @@ const Signup = () => {
             <label htmlFor="defaultFormRegisterEmailEx" className="grey-text">
               Your email
             </label>
-            <input
+            <input 
+              name = 'email'
               type="email"
               id="defaultFormRegisterEmailEx"
               className="form-control"
@@ -33,6 +48,7 @@ const Signup = () => {
               Your password
             </label>
             <input
+              name = 'password'
               type="password"
               id="defaultFormRegisterPasswordEx"
               className="form-control"
